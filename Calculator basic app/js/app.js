7e3b2894260d8b625b56704c963216842f02432f
MauricioCRojas <mauriciocc.rojas@gmail.com>
2022-01-05 13:29:32 -0300

    ...

--- Calculator basic app/js/app.js
+++ Calculator basic app/js/app.js
@@ -8,7 +8,7 @@ function sumar() {
   if (isNaN(resultado)) {
     resultado = "La operación no incluye números";
   }
-  document.getElementById("resultado").innerHTML = `Resultado: ${resultado}`;
+  document.getElementById("resultado").innerHTML = `Resultado Suma: ${resultado}`;
 }
 
 
@@ -20,5 +20,34 @@ function restar() {
     if (isNaN(resultado)) {
       resultado = "La operación no incluye números";
     }
-    document.getElementById("resultado").innerHTML = `Resultado: ${resultado}`;
+    document.getElementById("resultado").innerHTML = `Resultado Resta: ${resultado}`;
+  }
+
+  function multiplicar() {
+    const forma = document.getElementById("forma");
+    let operandoA = forma["operandoA"];
+    let operandoB = forma["operandoB"];
+    let resultado = parseInt(operandoA.value) * parseInt(operandoB.value);
+    if (isNaN(resultado)) {
+      resultado = "La operación no incluye números";
+    }
+    document.getElementById("resultado").innerHTML = `Resultado Producto: ${resultado}`;
+  }
+
+  function dividir() {
+    const forma = document.getElementById("forma");
+    let operandoA = forma["operandoA"];
+    let operandoB = forma["operandoB"];
+    let resultado = "";
+
+    if (operandoA.value=="" || operandoB.value=="") {
+      resultado = "La operación no incluye números";
+    }
+    else if(operandoB.value == 0){
+      resultado = `No es posible dividir por 0`;
+    }
+    else{
+      resultado = parseInt(operandoA.value) / parseInt(operandoB.value);
+    }
+    document.getElementById("resultado").innerHTML = `Resultado División: ${resultado}`;
   }
