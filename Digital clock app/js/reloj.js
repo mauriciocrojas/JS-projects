e9536aca53cc680d044a69d0e737a69af0665029
MauricioCRojas <mauriciocc.rojas@gmail.com>
2022-01-06 13:53:29 -0300

    ...

--- Digital clock app/js/reloj.js
+++ Digital clock app/js/reloj.js
@@ -25,3 +25,25 @@ const formatoHora = (hora)=>{
 }
 
 setInterval(mostrarReloj, 1000);
+
+
+const mostrarRelojUTC = () =>{
+    let fecha = new Date();
+    let hr = formatoHora(fecha.getUTCHours());
+    let min = formatoHora(fecha.getUTCMinutes());
+    let seg = formatoHora(fecha.getUTCSeconds());
+    document.getElementById("hora2").innerHTML = `${hr}:${min}:${seg}`;
+
+    const meses = ["Ene", "Feb", "Mar", "Abr", "Mayo", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"];
+    const dias = ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"];
+    let diaSemana = dias[fecha.getDay()];
+    let dia = fecha.getDate();
+    let mes = meses[fecha.getMonth()];
+    let anio = fecha.getFullYear();
+    let fechaTexto = `${diaSemana}, ${dia} ${mes}, ${anio}`;
+    document.getElementById("fecha2").innerHTML = fechaTexto;
+
+    document.getElementById("contenedor2").classList.toggle("animar");
+}
+
+setInterval(mostrarRelojUTC, 1000);
