badc3eac3d446d21c02f904450e42b144602814a
MauricioCRojas <mauriciocc.rojas@gmail.com>
2022-01-07 00:47:12 -0300

    ...

--- Login app/js/app.js
+++ Login app/js/app.js
@@ -1,6 +1,14 @@
 const mostrarCarga = ()=>{
 
-    document.getElementById("container").style.display = "block";
-    document.getElementById("carga").style.display = "block";
 
+
+    let usuario = document.getElementById("usuario");
+    let password = document.getElementById("password")
+    if(usuario.value == "" || password.value == ""){
+
+        alert("Faltan campos por completar");
+    }else{
+        document.getElementById("container").style.display = "block";
+        document.getElementById("carga").style.display = "block";
+    }
 }
